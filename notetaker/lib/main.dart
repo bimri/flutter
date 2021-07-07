69c1569756052fddfd52263a87ea276929a6cefd
bimri <bimri@outlook.com>
2021-07-07 23:54:12 +0300

    3 clean files - first draft

--- notetaker/lib/main.dart
+++ notetaker/lib/main.dart
@@ -1,65 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:notetaker/screens/note_list.dart';
+import 'package:notetaker/screens/note_detail.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(NoteTakerApp());
 }
 
-class MyApp extends StatelessWidget {
+class NoteTakerApp extends StatelessWidget {
+  const NoteTakerApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      title: 'NoteTaker',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: NoteList(),
     );
   }
 }
